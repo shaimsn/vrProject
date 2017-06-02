eb8ae08ef996fcd84b4df6e6e10e6d6ebb3de33c
Rehman Ali <themathtarehmanali@gmail.com>
2017-06-01 20:15:17 -0700

    Added FFT to HRIRCONTAINER

--- public/js/hrir_container.js
+++ public/js/hrir_container.js
@@ -64,6 +64,14 @@ var HrirContainer = function ( audioCtx ) {
 	/* Load the data to this.data */
 	this._load();
 
+	/*  */
+	// for ( var az of this.azimuths ) {
+	// 	for ( var el of this.elevations ) {
+	// 		console.log("az: ", az, "el: ", el, "data: ", this.data.L[ az ]);
+	// 		//console.log(this.data.R[ az ][ el ]);
+	// 	}
+	// }
+
 };
 
 
@@ -91,7 +99,6 @@ HrirContainer.prototype._load = function () {
 		this.data.R[ az ] = this._decodeHRIR( dirname + filenameR );
 
 	}
-
 
 };
 
@@ -226,6 +233,9 @@ HrirContainer.prototype.evaluate = function ( az, el ) {
 	var R_weighted = sumArrays(R00_weighted, sumArrays(R01_weighted,
 		sumArrays(R10_weighted, R11_weighted)));
 
+	console.log( cfft([1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0]) );
+	return { L: [1, 0], R: [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0] };
+
 	if (this.mode == true) {
 		/* Linear Interpolation */
 		console.log("Linear interpolation selected");
@@ -234,6 +244,10 @@ HrirContainer.prototype.evaluate = function ( az, el ) {
 	} else {
 		/* Nearest Neighbor */
 		console.log("Nearest neighbor noterpolation");
+
+		//console.log("az: ", az, "el: ", el, "data: ", this.data.L[ this.azimuths[a_idx] ]);
+				//console.log(this.data.R[ az ][ el ]);
+
 		return { L: this.data.L[ this.azimuths[a_idx] ][ this.elevations[e_idx] ],
 		       R: this.data.R[ this.azimuths[a_idx] ][ this.elevations[e_idx] ] };
 	}
@@ -369,4 +383,118 @@ function binarySearch( array, val ) {
 		return array[idx];
 	} */
 
+}
+
+/*
+complex fast fourier transform and inverse from
+http://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
+*/
+function icfft(amplitudes)
+{
+	var N = amplitudes.length;
+	var iN = 1 / N;
+
+	//conjugate if imaginary part is not 0
+	for(var i = 0 ; i < N; ++i)
+		if(amplitudes[i] instanceof Complex)
+			amplitudes[i].im = -amplitudes[i].im;
+
+	//apply fourier transform
+	amplitudes = cfft(amplitudes)
+
+	for(var i = 0 ; i < N; ++i)
+	{
+		//conjugate again
+		amplitudes[i].im = -amplitudes[i].im;
+		//scale
+		amplitudes[i].re *= iN;
+		amplitudes[i].im *= iN;
+	}
+	return amplitudes;
+}
+
+function cfft(amplitudes)
+{
+	var N = amplitudes.length;
+	if( N <= 1 )
+		return amplitudes;
+
+	var hN = N / 2;
+	var even = [];
+	var odd = [];
+	even.length = hN;
+	odd.length = hN;
+	for(var i = 0; i < hN; ++i)
+	{
+		even[i] = amplitudes[i*2];
+		odd[i] = amplitudes[i*2+1];
+	}
+	even = cfft(even);
+	odd = cfft(odd);
+
+	var a = -2*Math.PI;
+	for(var k = 0; k < hN; ++k)
+	{
+		if(!(even[k] instanceof Complex))
+			even[k] = new Complex(even[k], 0);
+		if(!(odd[k] instanceof Complex))
+			odd[k] = new Complex(odd[k], 0);
+		var p = k/N;
+		var t = new Complex(0, a * p);
+		t.cexp(t).mul(odd[k], t);
+		amplitudes[k] = even[k].add(t, odd[k]);
+		amplitudes[k + hN] = even[k].sub(t, even[k]);
+	}
+	return amplitudes;
+}
+
+/*
+basic complex number arithmetic from
+http://rosettacode.org/wiki/Fast_Fourier_transform#Scala
+*/
+function Complex(re, im)
+{
+	this.re = re;
+	this.im = im || 0.0;
+}
+Complex.prototype.add = function(other, dst)
+{
+	dst.re = this.re + other.re;
+	dst.im = this.im + other.im;
+	return dst;
+}
+Complex.prototype.sub = function(other, dst)
+{
+	dst.re = this.re - other.re;
+	dst.im = this.im - other.im;
+	return dst;
+}
+Complex.prototype.mul = function(other, dst)
+{
+	//cache re in case dst === this
+	var r = this.re * other.re - this.im * other.im;
+	dst.im = this.re * other.im + this.im * other.re;
+	dst.re = r;
+	return dst;
+}
+Complex.prototype.cexp = function(dst)
+{
+	var er = Math.exp(this.re);
+	dst.re = er * Math.cos(this.im);
+	dst.im = er * Math.sin(this.im);
+	return dst;
+}
+Complex.prototype.log = function()
+{
+	/*
+	although 'It's just a matter of separating out the real and imaginary parts of jw.' is not a helpful quote
+	the actual formula I found here and the rest was just fiddling / testing and comparing with correct results.
+	http://cboard.cprogramming.com/c-programming/89116-how-implement-complex-exponential-functions-c.html#post637921
+	*/
+	if( !this.re )
+		console.log(this.im.toString()+'j');
+	else if( this.im < 0 )
+		console.log(this.re.toString()+this.im.toString()+'j');
+	else
+		console.log(this.re.toString()+'+'+this.im.toString()+'j');
 }
